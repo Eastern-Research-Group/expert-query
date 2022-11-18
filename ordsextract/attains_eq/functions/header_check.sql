531780dd173fe3ef222b23d8338c8c5005f2b10d
Dziemiela <dziemiela.paul@epa.gov>
2022-11-18 09:12:59 -0500

    add header authorization code

---
+++ ordsextract/attains_eq/functions/header_check.sql
@@ -0,0 +1,31 @@
+﻿CREATE OR REPLACE FUNCTION header_check
+RETURN BOOLEAN
+AS
+   str_service_header VARCHAR2(4000 Char);
+   str_header_value   VARCHAR2(4000 Char);
+
+BEGIN
+
+   SELECT
+   a.header_value
+   INTO
+   str_header_value
+   FROM
+   attains_eq.header_authorization a;
+   
+   str_service_header := OWA_UTIL.GET_CGI_ENV('Api-Key');
+   
+   IF str_service_header IS NULL
+   THEN
+      RETURN FALSE;
+      
+   ELSIF str_service_header = str_header_value
+   THEN
+      RETURN TRUE;
+      
+   ELSE
+      RETURN FALSE;
+      
+   END IF;
+
+END header_check;
---
+++ ordsextract/attains_eq/tables/header_authorization.sql
@@ -0,0 +1,5 @@
+CREATE TABLE attains_eq.header_authorization(
+   header_value     VARCHAR2(40 Char)            NOT NULL
+  ,activation_date  DATE                         NOT NULL
+)
+TABLESPACE ATTAINS_DATA_ORCWATER;
